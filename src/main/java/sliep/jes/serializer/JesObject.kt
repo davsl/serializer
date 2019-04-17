15f4fb20d786a89890957d18ac5ccea89a97126b
dade.sliep <dade.sliep@gmail.com>
2019-04-17 10:20:34 +0200

    made JesObject Serializable

--- src/main/java/sliep/jes/serializer/JesObject.kt
+++ src/main/java/sliep/jes/serializer/JesObject.kt
@@ -1,8 +1,10 @@
 package sliep.jes.serializer
 
+import java.io.Serializable
+
 /**
  * Implement [JesObject] interface from your model class to let [JesSerializer] know that the type can be serialized/deserialized
  * @author sliep
  * @see JesSerializer
  */
-interface JesObject
+interface JesObject : Serializable
